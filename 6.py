fbf2ac4614263d7efd0a4a255195a8eeed8f592d
diixo <anatolii.podoba@gmail.com>
2025-08-07 04:23:28 +0300

    6.py updated comments

--- 6.py
+++ 6.py
@@ -1,30 +1,28 @@
 
 import stanza
 
-# Скачиваем модель
+
 stanza.download('en')
 
-# Создаем пайплайн с NER
 nlp = stanza.Pipeline(lang='en', processors='tokenize,ner')
 
-# Исходный текст
 text = "Barack Obama was born in Hawaii and became the president of the United States."
 
-# Анализируем
+# Analysis
 doc = nlp(text)
 
-# Посмотрим, что нашлось
 for ent in doc.ents:
     print(f"Text: {ent.text}\tType: {ent.type}")
 
-# Заменим сущности в тексте
+# Replace entities in text
 output = text
 
-# Чтобы не напутать индексы, идем от конца текста
+# To avoid mixing of indices, go from end to begin
 for ent in sorted(doc.ents, key=lambda e: e.start_char, reverse=True):
     start = ent.start_char
     end = ent.end_char
     output = output[:start] + ent.type + output[end:]
 
-print("\nЗаменённый текст:")
+
+print("\nReplaced тtext:")
 print(output)
