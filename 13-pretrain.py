04cb829174fdf829206f3b70fe67d43281c1119d
diixo <anatolii.podoba@gmail.com>
2025-08-07 04:22:39 +0300

    13-pretrain.py: initial commit

---
+++ 13-pretrain.py
@@ -0,0 +1,26 @@
+
+"""
+The Pile is a huge dataset (825 GB of text) created by EleutherAI as a corpus for pre-training large language models.
+It consists of 15 different subsets, including:
+
+📚 books3
+🌐 openwebtext2
+🧑‍🔬 pubmed
+🧵 stackexchange
+📄 arxiv
+🧑‍🏫 wiki
+📈 github
+...
+"""
+
+from datasets import load_dataset
+
+if __name__ == "__main__":
+
+    # pile = load_dataset("EleutherAI/pile", split="train") = 825Gb
+
+    ds = load_dataset("EleutherAI/pile", name="openwebtext2", split="train")
+
+    print(len(ds))
+
+    # ds.to_json("openwebtext2.jsonl", orient="records", lines=True)
