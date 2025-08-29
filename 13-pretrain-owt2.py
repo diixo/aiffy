3fe01c5cb339f41767fc8ff00c5b54fdde766072
diixo <anatolii.podoba@gmail.com>
2025-08-29 16:27:07 +0300

    13-pretrain-owt2.py: updated

--- 13-pretrain-owt2.py
+++ 13-pretrain-owt2.py
@@ -18,10 +18,24 @@ from datasets import load_dataset
 
 if __name__ == "__main__":
 
-    # pile = load_dataset("EleutherAI/pile", split="train") = 825Gb
+    #ds = load_dataset("EleutherAI/pile", split="train") #= 825Gb
 
-    ds = load_dataset("EleutherAI/pile", name="openwebtext2", split="train", trust_remote_code=True)
+    #ds = load_dataset("EleutherAI/pile", name="openwebtext2", split="train", trust_remote_code=True)
 
-    print(len(ds))
+    if True:
 
-    # ds.to_json("openwebtext2.jsonl", orient="records", lines=True)
+        ds = load_dataset("OpenAssistant/oasst1")
+        print(">>> oasst1:", len(ds), 24 * "*")
+
+        ds = load_dataset("blended_skill_talk")
+        print(">>> blended_skill_talk:", len(ds), 24 * "*")
+
+        # ds = load_dataset("topical_chat")
+
+        # print(">>> topical_chat:", len(ds), 24 * "*")
+
+        # looks like: https://huggingface.co/datasets/Skylion007/openwebtext
+        ds = load_dataset("openwebtext", split="train", trust_remote_code=True)
+        print(">>> openwebtext:", len(ds), 24 * "*")
+
+        # ds.to_json("openwebtext2.jsonl", orient="records", lines=True)
